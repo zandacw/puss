d00cfd5d80f7d6240821130432df239ec7e3e152
Zanda <zandacw@gmail.com>
2024-06-13 21:00:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Puss
+
+Puss is a simple command-line utility that integrates GNU Pass and a fuzzy finder to streamline copying passwords and retrieving OTP codes. It allows you to quickly find and copy passwords stored in your `.password-store` directory and optionally retrieve OTP codes if available.
+
+## Features
+
+- List and search passwords using fuzzy finder (similar to fzf) 
+- Copy selected password to clipboard
+- Display OTP code if available
+
+## Requirements
+
+- GNU Pass (`pass`)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/zandacw/puss.git
+   cd puss
+
+2. Install
+
+    ```bash
+    go install
+
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Contributing
+Contributions are welcome! Please feel free to submit a Pull Request.
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	ff "github.com/ktr0731/go-fuzzyfinder"
+)
+
+func runCommand(cmdName string, args ...string) (string, error) {
+	cmd := exec.Command(cmdName, args...)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func listPasswords() ([]string, error) {
+	listGpgFiles := "find ~/.password-store -type f -name '*.gpg'"
+	cleanGpgSuffix := "sed 's/\\.gpg$//'"
+	cleanPrefix := "sed 's#^'" + os.Getenv("HOME") + "'/.password-store/##'"
+	cmd := fmt.Sprintf("%s | %s | %s | awk '{print}'", listGpgFiles, cleanGpgSuffix, cleanPrefix)
+
+	pwds, err := runCommand("sh", "-c", cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(pwds, "\n"), nil
+}
+
+func copyPassword(path string) error {
+	res, err := runCommand("pass", "show", path, "-c")
+	if err != nil {
+		return err
+	} else if !strings.Contains(res, "Copied") {
+		return errors.New("ERROR: failed to copy")
+	}
+
+	return nil
+
+}
+
+func getOtp(path string) (string, error) {
+	res, err := runCommand("pass", "otp", path)
+	if err != nil {
+		return "", err
+	}
+	return res, err
+}
+
+func main() {
+	pswds, err := listPasswords()
+	if err != nil {
+		panic(err)
+	}
+
+	idx, err := ff.Find(pswds, func(i int) string { return pswds[i] })
+	if err != nil {
+		panic(err)
+	}
+
+	selected := pswds[idx]
+
+	err = copyPassword(selected)
+	if err != nil {
+		panic(err)
+	}
+
+	var output string
+	output += fmt.Sprintf("Copied \x1b[33m%s\x1b[0m to clipboard", selected)
+
+	otp, err := getOtp(selected)
+	if err == nil {
+		output += fmt.Sprintf(" with otp \x1b[34m%s\x1b[0m", otp)
+	}
+
+	fmt.Print(output)
+
+}
